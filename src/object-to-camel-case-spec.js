d2dd09d3aebd48e9eb978911d7b44e0476e0d5c8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-26 13:21:18 -0400

    initial code

---
+++ src/object-to-camel-case-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const objectToCamelCase = require('.')
+
+describe('object-to-camel-case', () => {
+  it('write this test', () => {
+    console.assert(objectToCamelCase, 'should export something')
+  })
+})
