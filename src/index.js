1d0086f1fb4ff7516f4cf805cdd67202dc4c8108
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-26 13:28:46 -0400

    feat(code): transforms given object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 'use strict'
 
+const camelCase = require('lodash.camelcase')
 
-module.exports = true
+function objectToCamelCase (object) {
+  Object.keys(object).forEach(key => {
+    const camelCased = camelCase(key)
+    if (camelCased !== key) {
+      object[camelCased] = object[key]
+      delete object[key]
+    }
+  })
+  return object
+}
 
+module.exports = objectToCamelCase
--- src/object-to-camel-case-spec.js
+++ src/object-to-camel-case-spec.js
@@ -1,11 +1,24 @@
 'use strict'
 
-
 /* global describe, it */
 const objectToCamelCase = require('.')
+const snapshot = require('snap-shot')
 
 describe('object-to-camel-case', () => {
-  it('write this test', () => {
-    console.assert(objectToCamelCase, 'should export something')
+  it('changes an object', () => {
+    const input = {
+      foo_bar: 42
+    }
+    const output = objectToCamelCase(input)
+    snapshot(output)
+  })
+
+  it('leaves camel cased properties unchanged', () => {
+    const input = {
+      fooBar: 42,
+      baz_foo: 'value'
+    }
+    const output = objectToCamelCase(input)
+    snapshot(output)
   })
 })
